a79cd08851784f8c52533f4af27b5a350d82d500
aalokshukla09 <shuklaraj0902@gmail.com>
2026-06-23 20:48:17 +0530

    adminAuthentication added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,6 +78,20 @@ export const loginUser = async (req, res) => {
 
 
 export const adminLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email + password, process.env.JWT_SECRET);
+            res.json({ success: true, token });
+        }
+        else {
+            res.json({ success: false, message: "Invalid email or password" });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 
 }
 
